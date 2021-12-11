0fdd1945a9d39dc3e984c77ee2ac74b137cc2b77
timcastillogill <timgillviola@gmail.com>
2021-12-11 16:26:13 +0000

    build: Add method and condition for buy 3 get one free for mangos

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -4,7 +4,7 @@ class Checkout
 
   def initialize(prices)
     @prices = prices
-    @total = 0
+    @total_cost = 0
   end
 
   def scan(item)
@@ -20,36 +20,42 @@ class Checkout
         half_price_offer(item, count)
       when :pineapple
         half_price_restricted_to_one_per_person(item, count)
+      when :mango
+        count % 4 == 0 ? three_get_one_free(item, count) : no_deal(item, count)
       else
         no_deal(item, count)
       end
     end
 
-    @total
+    @total_cost
   end
   
-  private
 
+  private
 
   def basket_count
     basket.inject(Hash.new(0)) { |items, item| items[item] += 1; items }
   end
 
   def no_deal(item, count)
-    @total += prices.fetch(item) * count
+    @total_cost += prices.fetch(item) * count
   end
 
   def two_for_one(item, count)
-      @total += prices.fetch(item) * (count / 2)
+      @total_cost += prices.fetch(item) * (count / 2)
+  end
+
+  def three_get_one_free(item, count)
+    @total_cost += prices.fetch(item) * (count - 1)
   end
 
   def half_price_restricted_to_one_per_person(item, count)
-    @total += (prices.fetch(item) / 2)
-    @total += (prices.fetch(item)) * (count - 1)
+    @total_cost += (prices.fetch(item) / 2)
+    @total_cost += (prices.fetch(item)) * (count - 1)
   end
 
   def half_price_offer(item, count)
-    @total += (prices.fetch(item) / 2) * count
+    @total_cost += (prices.fetch(item) / 2) * count
   end
 
   def basket
